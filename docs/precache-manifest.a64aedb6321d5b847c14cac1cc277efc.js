6c8887be68d725dc16b5b2437872fd0fd066e37b
erik172014 <73031650+erik172014@users.noreply.github.com>
2020-11-17 22:46:47 -0400

    Update 1

---
+++ docs/precache-manifest.a64aedb6321d5b847c14cac1cc277efc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10501cb6781329a38ebc6229b3f095e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b4bceb301f4f656fd8a",
+    "url": "/static/css/main.11d874ff.chunk.css"
+  },
+  {
+    "revision": "3ae1ec03d3c1fcb82845",
+    "url": "/static/js/2.c37393cb.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "/static/js/2.c37393cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b4bceb301f4f656fd8a",
+    "url": "/static/js/main.afc1ef02.chunk.js"
+  },
+  {
+    "revision": "c3f5f0c5a7b3c0a80425",
+    "url": "/static/js/runtime-main.f2d095ad.js"
+  },
+  {
+    "revision": "d3a8f115f144c07658c6ec16d878680a",
+    "url": "/static/media/TronLinkLogo.d3a8f115.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39158cb7d33a2242debf59b838d40055.js"
+  "/precache-manifest.a64aedb6321d5b847c14cac1cc277efc.js"
 );
 
 self.addEventListener('message', (event) => {
